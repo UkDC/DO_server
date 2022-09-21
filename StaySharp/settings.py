b46fbd555207647940af0756e485ff9aca302b05
UkDC(Mac) <109843268+UkDC@users.noreply.github.com>
2022-09-22 00:52:50 +0300

    start deploy 21.09.22

--- StaySharp/settings.py
+++ StaySharp/settings.py
@@ -130,9 +130,9 @@ LOGOUT_REDIRECT_URL = '/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Heroku: Обновление конфигурации базы данных из $DATAВASE_URL. import dj_database_url
-# dЬ_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(dЬ_from_env)
-DATABASES['default'] = dj_database_url.config(default='sqlite://db.sqlite3')
+dЬ_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(dЬ_from_env)
+# DATABASES['default'] = dj_database_url.config(default='sqlite://db.sqlite3')
 
 # Статичные файлы (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
